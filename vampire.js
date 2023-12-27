147db9adaac46ea6d2220eb27d15e74e4ee4e454
brittanyshelley <brittany.j.shelley@gmail.com>
2023-12-27 14:11:03 -0700

    Update package-lock.json and add new Vampire class

--- vampire.js
+++ vampire.js
@@ -10,35 +10,62 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
+
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
 
   }
+};
+  const original = new Vampire("Original", 1000);
+  const ansel = new Vampire("Ansel", 1100);
+  const bart = new Vampire("Bart", 1200);
+  const elgort = new Vampire("Elgort", 1300);
+  const sarah = new Vampire("Sarah", 1400);
+  const andrew = new Vampire("Andrew", 1500);
 
-  /** Stretch **/
 
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
-}
 
 module.exports = Vampire;
 
+class Node {
+
+  constructor(data) {
+    this.data = data;
+    this.parent = null;
+    this.children = [];
+  }
+
+  depthFirstTraversal() {
+
+    console.log(this); // 1
+
+    for (const childNode of this.children) {
+      childNode.depthFirstTraversal(); // 2
+    }
+  }
+}
